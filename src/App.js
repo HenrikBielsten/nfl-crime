1e097eb499a1c0e51fd25a36784fbf34d247b5a7
HenrikBielsten <h.bielsten@hotmail.se>
2018-04-25 11:16:32 +0200

    Clean up

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
       return (
         <div>
           <Form />
-
         </div>
     );
   }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -22,47 +22,59 @@ class Form extends Component {
 
   getTeams(event) {
     fetch(`http://nflarrest.com/api/v1/team?start_date=${this.state.start_date}&end_date=${this.state.end_date}`)
-      .then(res => res.json())
-      .then((result) => {
-        this.setState(state => ({teams: result }))
-        }
-        )
-  }
-
-  state = {
-    teams: [],
-    start_date: '2014-01-31',
-    end_date: '2016-01-31',
-  }
-
-  componentDidMount() {
-    this.getTeams();
+    .then(res => res.json())
+    .then((result) => {
+      this.setState(state => ({teams: result }))
     }
+  )
+}
 
- render() {
-   return (
-     <div>
+state = {
+  teams: [],
+  start_date: '2000-01-31',
+  end_date: '2016-01-31',
+}
 
-     <form onSubmit={this.handleSubmit}>
-       <label>
-         Select Start Date:
-         </label>
-         <select value={this.state.start_date} onChange={this.handleChange}>
-           <option value="2000-01-31">2000-01-31</option>
-           <option value="2001-01-31">2001-01-31</option>
-           <option value="2013-01-31">2013-01-31</option>
-           <option value="2014-01-31">2014-01-31</option>
-         </select>
-       <input type="submit" value="Submit" />
-     </form>
+componentDidMount() {
+  this.getTeams();
+}
 
-     <div className="teamsWrapper">
-       {this.state.teams.map((team, i) => <Team key={i} color="#30423B" name={team.Team_preffered_name} arrest={team.arrest_count}/>)}
-     </div>
+render() {
+  return (
+    <div>
 
-     </div>
-   );
- }
+    <form onSubmit={this.handleSubmit}>
+    <label>
+    Select Start Date:
+    </label>
+    <select value={this.state.start_date} onChange={this.handleChange}>
+    <option value="2000-01-31">2000-01-31</option>
+    <option value="2001-01-31">2001-01-31</option>
+    <option value="2002-01-31">2002-01-31</option>
+    <option value="2003-01-31">2003-01-31</option>
+    <option value="2004-01-31">2004-01-31</option>
+    <option value="2005-01-31">2005-01-31</option>
+    <option value="2006-01-31">2006-01-31</option>
+    <option value="2007-01-31">2007-01-31</option>
+    <option value="2008-01-31">2008-01-31</option>
+    <option value="2009-01-31">2009-01-31</option>
+    <option value="2010-01-31">2010-01-31</option>
+    <option value="2011-01-31">2011-01-31</option>
+    <option value="2012-01-31">2012-01-31</option>
+    <option value="2013-01-31">2013-01-31</option>
+    <option value="2014-01-31">2014-01-31</option>
+    <option value="2015-01-31">2015-01-31</option>
+    </select>
+    <input type="submit" value="Submit" />
+    </form>
+
+    <div className="teamsWrapper">
+    {this.state.teams.map((team, i) => <Team key={i} color="#30423B" name={team.Team_preffered_name} arrest={team.arrest_count}/>)}
+    </div>
+
+    </div>
+  );
+}
 }
 
 export default Form;
